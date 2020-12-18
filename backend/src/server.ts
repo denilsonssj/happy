ef2f45de5e0e6353cc799a6e0f78c45ece32de48
Name <email@email.com>
2020-12-18 08:34:21 -0300

    Clearing and updating backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
-const PORT = process.env.PORT || 3333;
+import './database/connection';
+
+const PORT = 3333;
 const app = express();
 app.use(cors);
 app.use(bodyParser.json());
