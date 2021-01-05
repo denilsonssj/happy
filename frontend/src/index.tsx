a2d836395eac140c829cc08d25795db2e7b4eac9
Name <email@email.com>
2021-01-05 13:58:52 -0300

    Adding new routes and root service api

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/global.css';
+import 'leaflet/dist/leaflet.css';
 
+import './styles/global.css';
 import Routes from './routes';
 
 ReactDOM.render(
